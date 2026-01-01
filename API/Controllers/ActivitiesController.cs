cae82f54affa789c6a5f9e0b84df2a8fd3be12e0
PensiveEagle <halljf96@gmail.com>
2026-01-01 15:38:21 +0000

    Initial Commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Runtime.CompilerServices;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
+namespace API.Controllers;
+
+public class ActivitiesController(AppDbContext context) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+    {
+        var activity = await context.Activities.FindAsync(id);
+
+        if (activity == null) return BadRequest();
+
+        return activity;
+    }
+}
