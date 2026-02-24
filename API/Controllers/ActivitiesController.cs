c00f4abb4015b44286beb86ef76144213bda9eb2
PensiveEagle <halljf96@gmail.com>
2026-02-24 08:23:13 +0000

    Added activity creation functionality
    Added handler for activity creation
    Added http post method to api controllers

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Runtime.CompilerServices;
+using Application.Activities.Commands;
 using Application.Activities.Queries;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.EntityFrameworkCore;
-using Persistance;
 
 namespace API.Controllers;
 
@@ -22,5 +17,11 @@ public class ActivitiesController : BaseApiController
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
         return await Mediator.Send(new GetActivityDetails.Query{Id = id});
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command{Activity = activity});
     }
 }
