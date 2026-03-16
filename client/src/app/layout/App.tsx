816dda0c9dc0d7d3548313b08e9ea7a57314add8
PensiveEagle <halljf96@gmail.com>
2026-03-16 20:48:56 +0000

    Styled the nav bar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline, List, ListItem, ListItemText } from "@mui/material";
+import { Container, CssBaseline, List, ListItem, ListItemText } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react"
 import NavBar from "./NavBar";
@@ -14,13 +14,15 @@ function App() {
     <>
       <CssBaseline />
       <NavBar />
-      <List>
-        {activities.map((activity) => (
-          <ListItem key={activity.id}>
-            <ListItemText>{activity.title}</ListItemText>
-          </ListItem>
-        ))}
-      </List>
+      <Container maxWidth="xl" sx={{mt: 3}}>
+        <List>
+          {activities.map((activity) => (
+            <ListItem key={activity.id}>
+              <ListItemText>{activity.title}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </Container>
     </>
   )
 }
