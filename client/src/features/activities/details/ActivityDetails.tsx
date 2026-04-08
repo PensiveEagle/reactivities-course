7afe71bae78242b0e7f31f6e2158f8bdfc2f9390
PensiveEagle <halljf96@gmail.com>
2026-04-08 13:52:08 +0100

    Added routing functionality for buttons
    Still doesn't work fully

--- client/src/features/activities/details/ActivityDetails.tsx
+++ client/src/features/activities/details/ActivityDetails.tsx
@@ -1,34 +1,27 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
-import { useActivities } from "../../../lib/hooks/useActivities"
+import { Link, useNavigate } from "react-router";
 
-type Props = {
-    selectedActivity: Activity
-    cancelSelectActivity: () => void
-    openForm: (id: string) => void
-}
+export default function ActivityDetails() {
+    const navigate = useNavigate();
+    const activity = {} as Activity;
 
-export default function ActivityDetails({selectedActivity, cancelSelectActivity, openForm}: Props) {
+    if (!activity) return <Typography>Loading...</Typography>
 
-  const {activities} = useActivities();
-  const activity = activities?.find(x => x.id === selectedActivity.id);
-
-  if (!activity) return <Typography>Loading...</Typography>
-
-  return (
-    <Card sx={{borderRadius: 3}}>
-        <CardMedia 
-            component="img"
-            src={`/images/categoryImages/${activity.category}.jpg`}
-        />
-        <CardContent>
-            <Typography variant="h5">{activity.title}</Typography>
-            <Typography variant="subtitle1" fontWeight="light">{activity.date}</Typography>
-            <Typography variant="body1">{activity.description}</Typography>
-        </CardContent>
-        <CardActions>
-            <Button onClick={() => openForm(activity.id)} color="primary">Edit</Button>
-            <Button onClick={cancelSelectActivity} color="inherit">Close</Button>
-        </CardActions>
-    </Card>
-  )
+    return (
+        <Card sx={{borderRadius: 3}}>
+            <CardMedia 
+                component="img"
+                src={`/images/categoryImages/${activity.category}.jpg`}
+            />
+            <CardContent>
+                <Typography variant="h5">{activity.title}</Typography>
+                <Typography variant="subtitle1" fontWeight="light">{activity.date}</Typography>
+                <Typography variant="body1">{activity.description}</Typography>
+            </CardContent>
+            <CardActions>
+                <Button component={Link} to={`/activities/${activity.id}`} color="primary">Edit</Button>
+                <Button onClick={() => navigate("/activities")} color="inherit">Close</Button>
+            </CardActions>
+        </Card>
+    )
 }
