15c528de0ad20f3f9339fb5b559ef630b7663867
PensiveEagle <halljf96@gmail.com>
2026-04-08 14:29:26 +0100

    Fixed bug
    Cleared form when jumping from edit to create

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -35,7 +35,9 @@ export default function ActivityForm() {
 
   return (
     <Paper sx={{borderRadius: 3, padding: 3}}>
-        <Typography variant="h5" gutterBottom color="primary">Create Activity</Typography>
+        <Typography variant="h5" gutterBottom color="primary">
+          {activity ? "Edit activity: " : "Create activity: "}
+        </Typography>
         <Box component="form" onSubmit={handleSubmit} display="flex" flexDirection="column" gap={3}>
             <TextField name="title" label="Title" defaultValue={activity?.title} />
             <TextField name="description" label="Description" defaultValue={activity?.description} multiline rows={3} />
