310e65afaf5d6c1747434b7f4408fbe0d8fd0aa8
PensiveEagle <halljf96@gmail.com>
2026-04-22 14:07:12 +0100

    Set up activity details to be a page
    Also created boilerplate for each of the page components

--- client/src/features/activities/details/ActivityDetails.tsx
+++ client/src/features/activities/details/ActivityDetails.tsx
@@ -1,9 +1,12 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
-import { Link, useNavigate, useParams } from "react-router";
+import { Grid2, Typography } from "@mui/material"
+import { useParams } from "react-router";
 import { useActivities } from "../../../lib/hooks/useActivities";
+import ActivityDetailsHeader from "./ActivityDetailsHeader";
+import ActivityDetailsInfo from "./ActivityDetailsInfo";
+import ActivityDetailsChat from "./ActivityDetailsChat";
+import ActivityDetailsSidebar from "./ActivityDetailsSidebar";
 
 export default function ActivityDetails() {
-    const navigate = useNavigate();
     const {id} = useParams();
     const {activity, isLoadingActivity} = useActivities(id);
 
@@ -12,20 +15,15 @@ export default function ActivityDetails() {
     if (!activity) return <Typography>Activity not found!</Typography>
 
     return (
-        <Card sx={{borderRadius: 3}}>
-            <CardMedia 
-                component="img"
-                src={`/images/categoryImages/${activity.category}.jpg`}
-            />
-            <CardContent>
-                <Typography variant="h5">{activity.title}</Typography>
-                <Typography variant="subtitle1" fontWeight="light">{activity.date}</Typography>
-                <Typography variant="body1">{activity.description}</Typography>
-            </CardContent>
-            <CardActions>
-                <Button component={Link} to={`/manage/${activity.id}`} color="primary">Edit</Button>
-                <Button onClick={() => navigate("/activities")} color="inherit">Close</Button>
-            </CardActions>
-        </Card>
+        <Grid2 container spacing={3}>
+            <Grid2 size={8}>
+                <ActivityDetailsHeader />
+                <ActivityDetailsInfo />
+                <ActivityDetailsChat />
+            </Grid2>
+            <Grid2 size={4}>
+                <ActivityDetailsSidebar />
+            </Grid2>
+        </Grid2>
     )
 }
