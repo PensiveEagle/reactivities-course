dd535f20644836e97b57c2ff049432a3ea090277
PensiveEagle <halljf96@gmail.com>
2026-07-06 21:44:22 +0100

    Used MobX for client side state
    Created a counter page and a loading state tracker using MobX

--- client/src/app/router/routes.tsx
+++ client/src/app/router/routes.tsx
@@ -4,6 +4,7 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import HomePage from "../../features/home/HomePage";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import Counter from "../../features/counter/Counter";
 
 export const router = createBrowserRouter([
     {
@@ -14,7 +15,8 @@ export const router = createBrowserRouter([
             { path: "activities", element: <ActivityDashboard /> },
             { path: "activities/:id", element: <ActivityDetails/>},
             { path: "createActivity", element: <ActivityForm key="create" /> },
-            { path: "manage/:id", element: <ActivityForm key="edit" />}
+            { path: "manage/:id", element: <ActivityForm key="edit" />},
+            { path: "counter", element: <Counter />}
         ]
     }
 ])
